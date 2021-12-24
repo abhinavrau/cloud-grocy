e2d6df3a96eba7e10e0312c34daa09261de818ce
Tom Preece <preece.thomas@icloud.com>
2021-12-24 16:59:49 +0000

    Need to use root user for new grocy image and path changed

--- backup/grocy-backup.sh
+++ backup/grocy-backup.sh
@@ -5,9 +5,9 @@ wget -O ./dbxcli https://github.com/dropbox/dbxcli/releases/download/v3.0.0/dbxc
 sudo chmod a+x ./dbxcli
 if ./dbxcli account; then
   mkdir -p backup
-  sudo docker exec grocy apk add sqlite
-  sudo docker exec grocy sqlite3 /www/data/grocy.db ".backup 'backup_grocy.db'"
-  sudo docker cp grocy:/www/public/backup_grocy.db backup/grocy.db
+  sudo docker exec --user=root grocy apk add sqlite
+  sudo docker exec --user=root grocy sqlite3 /var/www/data/grocy.db ".backup 'backup_grocy.db'"
+  sudo docker cp grocy:/var/www/backup_grocy.db backup/grocy.db
   # Ignore any errors here since directory may have already been created
   set +e
   ./dbxcli mkdir grocy_backup

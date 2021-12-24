f0295d14eab48268d7887b3e3a418259f2974ee3
Tom Preece <preece.thomas@icloud.com>
2021-12-24 16:57:47 +0000

    Make private key sensitive and changed host to use dns as ip might change

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -1,9 +1,10 @@
 output "host_ssh_key" {
   value = tls_private_key.ssh_key.private_key_pem
+  sensitive = true
 }
 
 output "grocy_host" {
-  value = aws_instance.grocy-ec2-instance.public_ip
+  value = aws_instance.grocy-ec2-instance.public_dns
 }
 
 output "grocy_url" {
